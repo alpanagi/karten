3c76f5e8b5fdede39552e9404a550985f11c1d01
Alexandros Panagiotakis <dev@alpanagi.com>
2026-09-13 18:54:47 +0200

    Read command line arguments.

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,6 @@
 const std = @import("std");
 
-pub fn main() void {}
+pub fn main(init: std.process.Init) !void {
+    const arguments = try init.minimal.args.toSlice(init.arena.allocator());
+    for (arguments) |argument| std.debug.print("{s}\n", .{argument});
+}
