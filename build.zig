bc44b4a1025ccf078b068c0025230644f51f4630
Alexandros Panagiotakis <dev@alpanagi.com>
2026-09-13 18:15:24 +0200

    Add toml as a dependency.

--- build.zig
+++ build.zig
@@ -4,12 +4,20 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const toml = b.dependency("toml", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
     const karten = b.addExecutable(.{
         .name = "karten",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
+            .imports = &.{
+                .{ .name = "toml", .module = toml.module("toml") },
+            },
         }),
     });
 
